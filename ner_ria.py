75ce6df980b832a4b4d38839c3f85ca8bc46662d
Ria Talsania <riatalsania0510@gmail.com>
2026-05-13 09:40:35 +0530

    new

--- ner_ria.py
+++ ner_ria.py
@@ -38,3 +38,13 @@ for i, word in enumerate(words):
 # Print BIO tags
 for word, tag in tags:
     print(word, "->", tag)
+
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp(text)
+
+print("=== PART A: spaCy ===\n")
+
+for ent in doc.ents:
+    print(ent.text, "->", ent.label_, "(" + spacy.explain(ent.label_) + ")")
